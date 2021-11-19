f0383d5b229f2b79ddea72d23874f0c7cca5ce70
NamanJain1902 <1902ben10@gmail.com>
2021-11-19 08:28:03 +0530

    set DEBUG_VALUE

--- LaundrySystem/settings.py
+++ LaundrySystem/settings.py
@@ -32,7 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 ALLOWED_HOSTS = ['tiet-laundry-management-system.herokuapp.com/']
 
